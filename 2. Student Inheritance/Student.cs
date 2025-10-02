12cc7be0f859f5eed888875ee29ccae6e24cc47c
Ice713 <icramzilruiz@gmail.com>
2025-10-02 09:43:06 +0800

    Working with Student Class

--- 2. Student Inheritance/Student.cs
+++ 2. Student Inheritance/Student.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace _2.Student_Inheritance
 {
-    internal class Student
+    internal class Student : Person
     {
-        
+        public string Course { get; set; }
+
+        public Student(string name, int age, string course) : base(name, age)
+        {
+            Course = course;
+        }
     }
 }
